2b49bda923b2677949002f2d742ee3ed4d5ab545
gustavost645 <gustavost_645@hotmail.com>
2021-09-22 19:54:23 -0300

    feature: inicio do projeto da api

---
+++ api-carwash/src/main/java/br/com/l2g/ApiCarwashApplication.java
@@ -0,0 +1,13 @@
+package br.com.l2g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarwashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarwashApplication.class, args);
+	}
+
+}
---
+++ api-carwash/src/test/java/br/com/l2g/ApiCarwashApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.l2g;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCarwashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
