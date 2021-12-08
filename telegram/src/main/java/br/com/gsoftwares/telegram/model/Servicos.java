3cc18ff425ae477afe7b9e2f2b62029795f404cd
gustavost645 <gustavost_645@hotmail.com>
2021-12-08 20:46:54 -0300

    Telegram

---
+++ telegram/src/main/java/br/com/gsoftwares/telegram/model/Servicos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.gsoftwares.telegram.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+/**
+ *
+ * @author lucas
+ */
+public class Servicos {
+
+    private Integer idServico;
+    private String nomeServico;
+}
