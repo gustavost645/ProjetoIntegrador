55710fbbbb2a7837654de81738d66d4416212c6d
gustavost645 <gustavost_645@hotmail.com>
2021-12-08 14:29:16 -0300

    fix: grupopermissao

--- api-carwash/src/main/java/br/com/l2g/entity/GrupoPermissao.java
+++ api-carwash/src/main/java/br/com/l2g/entity/GrupoPermissao.java
@@ -45,7 +45,7 @@ public class GrupoPermissao implements Serializable{
     @Column(name="gru_descricao")
     private String descricaoGrupoPermissao;
     
-    @OneToMany(cascade = CascadeType.MERGE)
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn (name = "per_grup_permission", referencedColumnName = "gru_codigo")
     private List<Permissao> listaPermissao;
     
